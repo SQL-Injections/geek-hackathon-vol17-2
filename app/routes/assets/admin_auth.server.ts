a99b8e2a2413cf7fa88ca1f6f6c45b07da8bc286
yoneyama <r.yoneyama03@gmail.com>
2024-11-03 13:04:10 +0900

    セッション名を変更

--- app/routes/assets/admin_auth.server.ts
+++ app/routes/assets/admin_auth.server.ts
@@ -1,49 +1,46 @@
-import { redirect } from "@remix-run/node";
-import { createCookieSessionStorage } from "@remix-run/node";
+import { redirect } from '@remix-run/node'
+import { createCookieSessionStorage } from '@remix-run/node'
 
 const cookieSessionStorage = createCookieSessionStorage({
     cookie: {
-        secure: process.env.NODE_ENV === "production",
+        name: 'admin__session',
+        secure: process.env.NODE_ENV === 'production',
         //一旦直で
-        secrets: ["kRyDcxor1dQgCXM4b4wtCeIoRi7dveedI/ZDUOwT96A="],
-        sameSite: "lax",
+        secrets: ['kRyDcxor1dQgCXM4b4wtCeIoRi7dveedI/ZDUOwT96A='],
+        sameSite: 'lax',
         maxAge: 30 * 24 * 60 * 60,
-        httpOnly: true,
-    },
-});
+        httpOnly: true
+    }
+})
 
 export async function createUserSession(userId: string, redirectPath: string) {
-    const session = await cookieSessionStorage.getSession();
-    session.set("usrId", userId);
-    session.set("role", "admin");
+    const session = await cookieSessionStorage.getSession()
+    session.set('usrId', userId)
+    session.set('role', 'admin')
     return redirect(redirectPath, {
         headers: {
-            "Set-Cookie": await cookieSessionStorage.commitSession(session),
-        },
-    });
+            'Set-Cookie': await cookieSessionStorage.commitSession(session)
+        }
+    })
 }
-
 
 export async function requireUserSession(request: Request) {
-    const data  = await getUserFromSession(request);
-    
+    const data = await getUserFromSession(request)
 
     if (!data) {
-        throw redirect("/admin_login");
+        throw redirect('/admin_login')
     }
 
-    return data;
+    return data
 }
 export async function getUserFromSession(request: Request) {
-    const session = await cookieSessionStorage.getSession(
-        request.headers.get("Cookie")
-    );
+    const session = await cookieSessionStorage.getSession(request.headers.get('Cookie'))
 
-    const usrId: string = session.get("usrId");
+    const usrId: string = session.get('usrId')
 
     if (!usrId) {
-        return null;
+        return null
     }
 
-    return {usrId};
+    return { usrId }
 }
--- app/routes/assets/student_auth.server.ts
+++ app/routes/assets/student_auth.server.ts
@@ -1,53 +1,50 @@
-import { redirect } from "@remix-run/node";
-import { createCookieSessionStorage } from "@remix-run/node";
+import { redirect } from '@remix-run/node'
+import { createCookieSessionStorage } from '@remix-run/node'
 
 const cookieSessionStorage = createCookieSessionStorage({
     cookie: {
-        secure: process.env.NODE_ENV === "production",
+        name: 'student__session',
+        secure: process.env.NODE_ENV === 'production',
         //一旦直で
-        secrets: ["kRyDcxor1dQgCXM4b4wtCeIoRi7dveedI/ZDUOwT96A="],
-        sameSite: "lax",
+        secrets: ['kRyDcxor1dQgCXM4b4wtCeIoRi7dveedI/ZDUOwT96A='],
+        sameSite: 'lax',
         maxAge: 30 * 24 * 60 * 60,
-        httpOnly: true,
-    },
-});
+        httpOnly: true
+    }
+})
 
 export async function createUserSession(userId: string, classId: string, usrName: string, redirectPath: string) {
-    const session = await cookieSessionStorage.getSession();
-    session.set("usrId", userId);
-    session.set("role", "student");
-    session.set("classId", classId);
-    session.set("usrName", usrName);
+    const session = await cookieSessionStorage.getSession()
+    session.set('usrId', userId)
+    session.set('role', 'student')
+    session.set('classId', classId)
+    session.set('usrName', usrName)
     return redirect(redirectPath, {
         headers: {
-            "Set-Cookie": await cookieSessionStorage.commitSession(session),
-        },
-    });
+            'Set-Cookie': await cookieSessionStorage.commitSession(session)
+        }
+    })
 }
-
 
 export async function requireUserSession(request: Request) {
-    const data  = await getUserFromSession(request);
-    
+    const data = await getUserFromSession(request)
 
     if (!data) {
-        throw redirect("/login_students");
+        throw redirect('/login_students')
     }
 
-    return data;
+    return data
 }
 export async function getUserFromSession(request: Request) {
-    const session = await cookieSessionStorage.getSession(
-        request.headers.get("Cookie")
-    );
+    const session = await cookieSessionStorage.getSession(request.headers.get('Cookie'))
 
-    const usrId: string = session.get("usrId");
-    const classId: string = session.get("classId");
-    const usrName: string = session.get("usrName");
+    const usrId: string = session.get('usrId')
+    const classId: string = session.get('classId')
+    const usrName: string = session.get('usrName')
 
     if (!usrId) {
-        return null;
+        return null
     }
 
-    return {usrId, usrName, classId};
+    return { usrId, usrName, classId }
 }
